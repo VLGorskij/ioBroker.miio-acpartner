7caa2623577a6ac49d69d142eb5cd8db863d6a19
VLGorskij <45288874+VLGorskij@users.noreply.github.com>
2020-03-02 10:14:55 +0300

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,126 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    "title": {
+        "en": "Adapter for controlling air conditioning via AC partner via miio protocol",
+        "de": "Adapter zur Steuerung der Klimaanlage über einen AC-Partner über das Miio-Protokoll",
+        "ru": "Адаптер для управления кондиционером через АС Partner по протоколу miio",
+        "pt": "Adaptador para controle de ar condicionado via parceiro CA via protocolo miio",
+        "nl": "Adapter voor het regelen van airconditioning via AC-partner via miio-protocol",
+        "fr": "Adaptateur pour contrôler la climatisation via un partenaire AC via le protocole miio",
+        "it": "Adattatore per il controllo del condizionamento dell'aria tramite partner AC tramite protocollo miio",
+        "es": "Adaptador para controlar el aire acondicionado a través de AC partner a través del protocolo miio",
+        "pl": "Adapter do sterowania klimatyzacją za pośrednictwem partnera AC za pomocą protokołu miio"
+    },
+    "IP address:": {
+        "en": "IP address:",
+        "de": "IP Adresse:",
+        "ru": "IP адрес:",
+        "pt": "Endereço de IP:",
+        "nl": "IP adres:",
+        "fr": "Adresse IP:",
+        "it": "Indirizzo IP:",
+        "es": "Dirección IP:",
+        "pl": "Adres IP:"
+    },
+    "connection settings": {
+        "en": "connection settings",
+        "de": "Verbindungseinstellungen",
+        "ru": "Настройки соединения",
+        "pt": "configurações de conexão",
+        "nl": "verbindingsinstellingen",
+        "fr": "paramètres de connexion",
+        "it": "impostazioni di connessione",
+        "es": "configuración de conexión",
+        "pl": "ustawienia połączenia"
+    },
+    "additional settings": {
+        "en": "additional settings",
+        "de": "zusätzliche Einstellungen",
+        "ru": "дополнительные настройки",
+        "pt": "Configurações adicionais",
+        "nl": "aanvullende instellingen",
+        "fr": "paramètres additionnels",
+        "it": "altre impostazioni",
+        "es": "ajustes adicionales",
+        "pl": "dodatkowe ustawienia"
+    },
+    "Invalid token length. Expected 32 or 96 HEX chars.": {
+        "en": "Invalid token length. Expected 32 or 96 HEX chars.",
+        "de": "Ungültige Tokenlänge Erwartete 32 oder 96 HEX-Zeichen.",
+        "ru": "Недопустимая длина токена. Ожидаемые 32 или 96 символов HEX.",
+        "pt": "Comprimento de token inválido. Esperava 32 ou 96 caracteres HEX.",
+        "nl": "Ongeldige tokenlengte. Verwachte 32 of 96 HEX-tekens.",
+        "fr": "Longueur de jeton incorrecte Caractères HEX attendus 32 ou 96.",
+        "it": "Lunghezza del token non valida. Previsti 32 o 96 caratteri HEX.",
+        "es": "Longitud de token inválida. Se esperan 32 o 96 caracteres HEX.",
+        "pl": "Nieprawidłowa długość tokenu. Oczekiwano 32 lub 96 znaków HEX."
+    },
+    "Miio-acpartner adapter settings": {
+        "en": "Miio-acpartner adapter settings",
+        "de": "Miio-acpartner Adaptereinstellungen",
+        "ru": "Настройки адаптера miio-acpartner",
+        "pt": "Configurações do adaptador Miio-accpartner",
+        "nl": "Instellingen voor Miio-acpartner-adapter",
+        "fr": "Paramètres de l'adaptateur Miio-acpartner",
+        "it": "Impostazioni dell'adattatore Miio-acpartner",
+        "es": "Configuración del adaptador Miio-Acpartner",
+        "pl": "Ustawienia adaptera Miio-Acpartner"
+    },
+    "Own port:": {
+        "en": "Own port:",
+        "de": "Eigener Port:",
+        "ru": "Собственный порт:",
+        "pt": "Porta própria:",
+        "nl": "Eigen poort:",
+        "fr": "Propre port:",
+        "it": "Porto proprio:",
+        "es": "Puerto propio:",
+        "pl": "Własny port:"
+    },
+    "Request Intervall": {
+        "en": "Request Intervall in ms ",
+        "de": "Abfrage Intervall in ms ",
+        "ru": "Интервал запросов в мс",
+        "pt": "Solicitar Intervall em ms",
+        "nl": "Interval aanvragen in ms",
+        "fr": "Demander Intervall en ms",
+        "it": "Richiesta Intervall in ms",
+        "es": "Solicitar Intervall en ms",
+        "pl": "Popro? Intervall w ms"
+    },
+    "Token": {
+        "en": "token",
+        "de": "Token",
+        "ru": "Токен",
+        "pt": "Símbolo",
+        "nl": "token",
+        "fr": "Jeton",
+        "it": "Gettone",
+        "es": "Simbólico",
+        "pl": "Znak"
+    },
+    "AC Partner Port:": {
+        "en": "AC Partner Port: ",
+        "de": "AC Partner Port: ",
+        "ru": "Порт AC Partner:",
+        "pt": "Porta do parceiro AC:",
+        "nl": "AC-partnerpoort:",
+        "fr": "Port partenaire AC:",
+        "it": "Porta partner AC:",
+        "es": "AC Partner Port:",
+        "pl": "AC Partner Port:"
+    },
+    "settings": {
+        "en": "settings",
+        "de": "Einstellungen",
+        "ru": "настройки",
+        "pt": "configurações",
+        "nl": "instellingen",
+        "fr": "paramètres",
+        "it": "impostazioni",
+        "es": "ajustes",
+        "pl": "ustawienia"
+    }
+};
